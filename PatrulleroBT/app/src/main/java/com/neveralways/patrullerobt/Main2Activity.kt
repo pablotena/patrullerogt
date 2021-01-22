2876eac111b25af1d62072c064aa13b4edac8524
pablotena <pablotenasanchez@gmail.com>
2021-01-22 21:36:58 +0100

    Nuevos botones de dirección y funcionalidad
    Se han añadido nuevos botones de dirección y se ha creado la funcionalidad para enviar por bluetooth el movimiento. Se ha utilizado un Runnable para poder mantener pulsado el botón de dirección y que el vehículo se mantenga en movimiento.
    
    También se han traducido algunas variables al español. A partir de ahora todo se pondrá en español para mantener un orden.

--- PatrulleroBT/app/src/main/java/com/neveralways/patrullerobt/Main2Activity.kt
+++ PatrulleroBT/app/src/main/java/com/neveralways/patrullerobt/Main2Activity.kt
@@ -2,24 +2,27 @@ package com.neveralways.patrullerobt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.hp.bluetoothjhr.BluetoothJhr
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.coroutines.*
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
 import kotlin.concurrent.thread
 
 class Main2Activity : AppCompatActivity() {
 
     lateinit var bluetoothJhr: BluetoothJhr
-    var hiloEjecutando:Boolean = true
+    var changed : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         bluetoothJhr = BluetoothJhr(MainActivity::class.java, this)
-        hiloEjecutando = true
 
         enviar.setOnClickListener {
             var mensaje = editor.text.toString()
@@ -27,6 +30,52 @@ class Main2Activity : AppCompatActivity() {
         }
 
         // Para recibir datos habría que hacer una corrutina con bluetoothJhr.Rx()
+
+        val adelanteBoton = findViewById<Button>(R.id.btnAdelante)
+        val derechaBoton = findViewById<Button>(R.id.btnDerecha)
+        val atrasBoton = findViewById<Button>(R.id.btnAtras)
+        val izquierdaBoton = findViewById<Button>(R.id.btnIzquierda)
+
+
+        adelanteBoton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+                // Función que mueve el vehículo en base al botón pulsado.
+                moveIt(v, event)
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
+
+        derechaBoton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+                // Función que mueve el vehículo en base al botón pulsado.
+                moveIt(v, event)
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
+
+        atrasBoton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+                // Función que mueve el vehículo en base al botón pulsado.
+                moveIt(v, event)
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
+
+        izquierdaBoton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+                // Función que mueve el vehículo en base al botón pulsado.
+                moveIt(v, event)
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
 
     }
 
@@ -39,6 +88,65 @@ class Main2Activity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         bluetoothJhr.CierraConexion()
-        hiloEjecutando = false
+    }
+
+    fun moveIt(v : View?, event : MotionEvent?) {
+        var mHandler : Handler = Handler()
+        var mAction = object : Runnable {
+            override fun run() {
+                //consola.text = "${consola.text} hola"
+                var stringSent = ""
+
+                if(changed) {
+                    mHandler.removeCallbacks(this)
+                    stringSent = "P"
+                    bluetoothJhr.Tx(stringSent)
+                    Log.d("Info", "He dejado de enviar ${v?.id}. Termino con una ${stringSent}")
+                    consola.text = "Se ha enviado una ${stringSent}. Nos detenemos."
+                } else {
+
+                    when(v?.id) {
+                        R.id.btnAdelante -> {
+                            stringSent = "W"
+                        }
+                        R.id.btnDerecha -> {
+                            stringSent = "D"
+                        }
+                        R.id.btnAtras -> {
+                            stringSent = "S"
+                        }
+                        R.id.btnIzquierda -> {
+                            stringSent = "A"
+                        }
+                        else -> {
+                            Log.d("Info", "No se reconoce ese botón")
+                        }
+                    }
+
+                    bluetoothJhr.Tx(stringSent)
+                    Log.d("Info", "Se ha enviado ${stringSent} por TX con ${v?.id}")
+                    consola.text = stringSent
+
+                    mHandler.postDelayed(this, 50)
+                }
+            }
+        }
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                changed = false
+                mHandler.removeCallbacks(mAction)
+                mHandler.postDelayed(mAction, 1)
+                consola.text = "HE PULSADO"
+                Log.d("Test", "PULSO ${v?.id}")
+            }
+            MotionEvent.ACTION_UP -> {
+                mHandler.removeCallbacks(mAction)
+                changed = true
+
+
+                Log.d("Test", "LEVANTO ${R.id.btnAdelante}")
+            }
+        }
     }
 }
